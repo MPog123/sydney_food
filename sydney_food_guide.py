81e3036421d73dc95771804dcd4cdd8c93831448
Michael Pogrebinsky <michaelpogrebinsky123@gmail.com>
2025-06-04 23:44:49 +1000

    Trying HTML display for 'Search by Filter' table

--- sydney_food_guide.py
+++ sydney_food_guide.py
@@ -217,6 +217,6 @@ filtered_df = filtered_df[filtered_df["Price"].isin(selected_prices)]
 df_display = filtered_df[["Name", "Category", "Suburb", "Price"]].sort_values("Name").reset_index(drop=True)
 df_display.index = df_display.index + 1  # Make index 1-based
 # st.dataframe(df_display, use_container_width=True, height=500)
-st.data_editor(df_display, use_container_width=True, hide_index=True, disabled=True)
+# st.data_editor(df_display, use_container_width=True, hide_index=True, disabled=True)
 # Convert to HTML table without index
-# st.markdown(df_display.to_html(index=False), unsafe_allow_html=True)
+st.markdown(df_display.to_html(index=False), unsafe_allow_html=True)
